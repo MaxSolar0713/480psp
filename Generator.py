b7e45ac982e818ba6c056ee7bd544bbccf34cc36
MaxSolar0713 <63810567+MaxSolar0713@users.noreply.github.com>
2023-10-21 11:24:15 -0700

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,16 +1,21 @@
+# TITLE SEQUENCE IS HERE
 print ("")
 print ("PSP ffmpeg Encoder Script 1.0")
 print ("By MaxSolar0713")
 print ("")
+# This part acquirest the input and output locations of the file, so it can get the important stuff out of the way.
 I = input("Please enter location of input file: ")
 O = input("Please enter location of output file: ")
+# Asks if you want to change bitrates.
 M = input("Would you like to change advanced settings? [y/n] ")
 if M == "y":
     A = input("What bitrate for audio? (kbps) ")
     V = input("What bitrate for video? (kbps) ")
 else:
+    # DEFAULT BITRATES ARE 2.5Mbps for video, and 256kbps for audio.
     A = 256
     V = 2500
+# THIS IS WHERE THE COMMAND IS PUT OUT.
 print ("")
 print ("Here is your command!")
 print ("")
